876a44965ce5b205972b86d00b9910e8aecbd1e4
SubrotoRoy <subrotoroy007@gmail.com>
2021-02-19 22:37:45 +0530

    commit after adding some additional comments

--- methodsets/main.go
+++ methodsets/main.go
@@ -26,10 +26,19 @@ func main() {
 		last:  "Holmes",
 		age:   32,
 	}
+	var pp1 *person
+	pp1 = &p1
+
+	p1.speak()
+	pp1.speak()
 	//saySomething(p1)
-	saySomething(&p1)
+	saySomething(pp1)
 }
 
 //saySomething(p1) is not allowed. This is because speak method has a pointer receiver.
 //cannot use p1 (variable of type person) as human value
 //in argument to saySomething: missing method speak (speak has pointer receiver)
+
+/*
+
+*/
